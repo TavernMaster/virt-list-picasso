bd03404aaa0c695be7d6d8cae7185d9100b1cc2e
TavernMaster <dimasamusch@yandex.ru>
2023-10-12 14:34:52 +0300

    Build

--- src/Store/index.js
+++
@@ -1,9 +0,0 @@
-import {configureStore} from '@reduxjs/toolkit'
-import posts from '../Posts/slices/'
-import {postsApi} from '../Posts/services/index'
-
-export default configureStore({
-	reducer: {posts, [postsApi.reducerPath]: postsApi.reducer},
-	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(postsApi.middleware),
-	devTools: process.env.NODE_ENV == 'development' ? true : false,
-})
